5d079f15563a7de77c5f26695834d6a204ea3e7a
Globe-Eater <bullokl@ostatemail.okstate.edu>
2021-02-04 20:38:51 -0600

    Ctypes integration made.

--- image_processing/Standardize_Labels.py
+++ image_processing/Standardize_Labels.py
@@ -1,18 +1,31 @@
 import os
 import ctypes
+from ctypes import *
+libstdimg = CDLL("./libstdimg.so")
 
-input_dir = "/Users/kellenbullock/Desktop/Natural_Resources_Project/datasets/Stage_2/"
+# Calling the C function to check connection
+libstdimg.connect()
+
+input_dir = "/Users/kellenbullock/Desktop/Natural_Resources_Project/datasets/Stage_1/"
 
 input_img_paths = sorted(
         [
-             os.path.join(input_data_path, fname)
+             os.path.join(input_dir, fname)
              for fname in os.listdir(input_dir)
              if fname.endswith(".jpg")
          ]
     )
-standardizer = ctypes.CDLL("standard_process.so")
-standardizer.standardize.argtypes = [ctypes.c_wchar_p]
+image_standardize = libstdimg.std_image
+image_standardize.argtypes = (ctypes.c_char_p, ctypes.c_char_p)
 
-for x in input_img_paths:
-    standardizer.standardize(x, "/Users/kellenbullock/Desktop/Natural_Resources_Project/datasets/Raw_Sliced_Images/" + str(x[77:-4]) + "_" + str(n) + ".png")
-
+for i in input_img_paths:
+    path_1 = i
+    print(i)
+    print("path 1 = %s", path_1)
+    path_2 = '/Users/kellenbullock/Desktop/Natural_Resources_Project/datasets/Stage_2/' + str(i[72:-4]) + "_" + '.png'
+    print("path2 = %s", path_2)
+    print("")
+    path_1 = path_1.encode('utf-8')
+    path_2 = path_2.encode('utf-8')
+    image_standardize(path_1, path_2)
+    print("done.")
