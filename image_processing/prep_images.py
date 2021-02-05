5f78b3c4cfc0d30356c58b74e1fe64a61108d5cc
Globe-Eater <bullokl@ostatemail.okstate.edu>
2021-02-04 20:43:59 -0600

    Preprocessing file started.

---
+++ image_processing/prep_images.py
@@ -0,0 +1,10 @@
+import os
+
+
+def main():
+    """ Prepares all images for ML model training."
+    Standardize_labels()
+    Image_Slicer()
+    print("Prep complete.")
+if __name__ == '__main__':
+    main()
